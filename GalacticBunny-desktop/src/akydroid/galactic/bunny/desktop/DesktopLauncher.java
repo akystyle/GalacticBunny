31f8f3997c11fc5f8156feb55b565ba0c6f6d909
GOELS <GOELS@GOELS-PC>
2015-09-22 23:53:13 +0530

    Things running

--- GalacticBunny-desktop/src/akydroid/galactic/bunny/desktop/DesktopLauncher.java
+++ GalacticBunny-desktop/src/akydroid/galactic/bunny/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import akydroid.galactic.bunny.GalacticBunnyMainGameClass;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "AkyDroid's app";
+		config.height = 408;
+		config.width = 272;
 		new LwjglApplication(new GalacticBunnyMainGameClass(), config);
 	}
 }
